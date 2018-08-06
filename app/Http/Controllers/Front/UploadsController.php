3955cf463256c7f4631c99007ba68b9bfaa83bcc
shine <napestershine@gmail.com>
2018-08-06 22:55:31 +0530

    fixed upload API

--- app/Http/Controllers/Front/UploadsController.php
+++ app/Http/Controllers/Front/UploadsController.php
@@ -10,8 +10,9 @@
 
 namespace App\Http\Controllers\Front;
 
-
-use App\helpers\Utility;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Str;
+use Intervention\Image\ImageManagerStatic as Image;
 use App\Http\Controllers\BaseController;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -28,6 +29,23 @@ class UploadsController extends BaseController
      *
      * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\JsonResponse
+     *
+     * @SWG\Post(path="/upload",
+     *   tags={"Image Upload"},
+     *   summary="Uploads a new image in server date wise",
+     *   description="Store a new image in public directory.",
+     *   operationId="uploadImage",
+     *   consumes={"multipart/form-data"},
+     *   produces={"application/json"},
+     *   @SWG\Parameter(
+     *     description="image file to upload",
+     *     in="formData",
+     *     name="file",
+     *     required=true,
+     *     type="file"
+     *   ),
+     *   @SWG\Response(response="200", description="successful operation")
+     * )
      */
     public function fileUpload(Request $request): JsonResponse
     {
@@ -36,12 +54,9 @@ class UploadsController extends BaseController
             $this->validate($request, [
                 'file' => 'required'
             ]);
-
             $dir = 'upload/' . date('Y-m', time()) . '/';
-            $imageName = Utility::uploadImage($request->file, $dir);
-            $response = [
-                'data' => url($dir . Utility::uploadImage($request->file, $dir, 80, 80, $imageName))
-                ];
+            $path = static::uploadImage($request->file, $dir);
+            $response = compact('path');
 
         } catch (ValidationException $e) {
             $statusCode = 400;
@@ -53,4 +68,36 @@ class UploadsController extends BaseController
 
         return response()->json($response, $statusCode);
     }
+
+    /**
+     * Upload an image
+     *
+     * @param UploadedFile $image
+     * @param $dir
+     * @param int $width
+     * @param int $height
+     * @param null $imgName
+     * @return string $path
+     */
+    public static function uploadImage(UploadedFile $image, $dir, $width = 0, $height = 0, $imgName = null)
+    {
+        // if directory doesn't exist then create it
+        if (!file_exists($dir)) {
+            mkdir($dir, 0777, true);
+        }
+        if ($dir[strlen($dir) - 1] !== '/') {
+            $dir .= '/';
+        }
+
+        $img = Image::make($image);
+        if ($width > 0 && $height > 0) {
+            $img->resize($width, $height);
+        }
+        $ext = pathinfo($image->getClientOriginalName(), PATHINFO_EXTENSION);
+        $imgName = ($imgName === null) ? Str::slug(Str::random(32)) . '.' . $ext : $imgName;
+        $path = $dir . $imgName;
+        $img->save($path);
+        return  url('/'.$path);
+    }
+
 }
